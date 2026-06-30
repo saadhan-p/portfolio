4eb03c94008a3e302e092dab5e24a3250cc82cd2
saadhan <saadhan275@gmail.com>
2026-06-30 21:35:17 +0530

    fix: Parse PORT as number to resolve TS type check error

--- server.ts
+++ server.ts
@@ -8,7 +8,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = process.env.PORT || 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   // Standard middleware
   app.use(express.json());
